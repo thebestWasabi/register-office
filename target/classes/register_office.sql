2cfc64f74be950a7bb649b340b0a1ebd92e58a9a
thebestWasabi <100003190+thebestWasabi@users.noreply.github.com>
2022-11-12 08:14:32 +0300

    Delete target/classes directory

--- target/classes/register_office.sql
+++
@@ -1,8 +0,0 @@
-drop table if exist person;
-
-create table person (
-    person_id serial not null,
-    first_name varchar(100) not null,
-    last_name varchar(100) not null,
-    primary key (person_id)
-);
