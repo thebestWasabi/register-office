76c8129dbb6d0866b44133cf70d666b7e82e4954
thebestWasabi <openpw@icloud.com>
2022-11-21 17:07:42 +0300

    0.0.3

--- src/main/java/edu/javalesson/register/domain/PersonFemale.java
+++ src/main/java/edu/javalesson/register/domain/PersonFemale.java
@@ -1,6 +1,25 @@
 package edu.javalesson.register.domain;
 
-public class PersonFemale {
+import java.util.List;
 
+public class PersonFemale extends Person {
 
+    private List<MarriageCertificate> marriageCertificates;
+    private List<BirthCertificate> birthCertificates;
+
+    public List<MarriageCertificate> getMarriageCertificates() {
+        return marriageCertificates;
+    }
+
+    public void setMarriageCertificates(List<MarriageCertificate> marriageCertificates) {
+        this.marriageCertificates = marriageCertificates;
+    }
+
+    public List<BirthCertificate> getBirthCertificates() {
+        return birthCertificates;
+    }
+
+    public void setBirthCertificates(List<BirthCertificate> birthCertificates) {
+        this.birthCertificates = birthCertificates;
+    }
 }
